6d31623e553f7c85f40ccbcd711c0bbaa320124c
ddvniele <ddvniele@gmail.com>
2024-09-06 01:12:02 +0200

    Initial Commit

---
+++ Sssymbols-iOS/Sssymbols_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sssymbols_iOSApp.swift
+//  Sssymbols-iOS
+//
+//  Created by dan on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Sssymbols_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
